8a4b8d7f81c8d01ef11dcb1cc4e975cd16fa7be0
YarRus12 <yaroslav.russu@mail.ru>
2023-01-15 09:28:28 +1000

    Создан API на для Project и ToDo

--- library/authors/models.py
+++ library/authors/models.py
@@ -5,7 +5,37 @@ from django.db import models
 
 class Author(models.Model):
     objects = None
-    id = models.UUIDField(default=uuid4, primary_key=True)
+    id = models.UUIDField(primary_key=True)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+
+
+class Biograthy(models.Model):
+    text = models.TextField()
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=32)
+    author = models.ManyToManyField(Author)
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=32)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=32)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=32)
+    author = models.ManyToManyField(Author)
+
+
+class ToDo(models.Model):
+    record = models.CharField(max_length=32)
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
--- library/library/settings.py
+++ library/library/settings.py
@@ -126,3 +126,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+# Отображение фреймворка
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.AdminRenderer',
+
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        ]
+                }
